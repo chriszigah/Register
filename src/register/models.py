6fcde361159106d1f3a6b6da9b708ef5c9db157d
Christian Zigah <zigahchristian@yahoo.com>
2024-05-02 21:18:06 +0000

    Added UUID as primary key for records

--- src/register/models.py
+++ src/register/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
-
-
+import uuid
 
 
 # Create your models here.
 
 class Record(models.Model):
+    record_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     creation_date = models.DateTimeField(auto_now_add=True)
     department = models.ForeignKey('Department', on_delete=models.SET_NULL, blank=True, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE,  blank=True, null=True)
@@ -19,7 +19,6 @@ class Record(models.Model):
     country = models.CharField(max_length=20)
     avatar = models.ImageField(default='avatar.png', blank=True)
     
-
     def __str__(self):
         return self.first_name
 
