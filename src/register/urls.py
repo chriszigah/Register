713abe51e01bf5848cb90a63affbd2edf5e1612c
Christian Zigah <zigahchristian@yahoo.com>
2024-05-02 21:19:39 +0000

    Updated Class Names

--- src/register/urls.py
+++ src/register/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views 
 
-from . views import register, home, dashboard, create_record, update_record, view_record, delete_record
+from . views import Register, Home, Dashboard, CreateRecord, UpdateRecord, ViewRecord, DeleteRecord
 
 from django.contrib.auth import views as auth_views
 
@@ -9,17 +9,17 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path("", home.as_view(), name=""),
-    path("register/", register.as_view(), name="register" ),
+    path("", Home.as_view(), name=""),
+    path("register/", Register.as_view(), name="register" ),
     #path("login", login.as_view(), name="login"),
     path('login/', auth_views.LoginView.as_view(template_name='register/login.html'), name='login'),
     path("logout", auth_views.LogoutView.as_view(template_name='register/index.html'), name='logout'),
     #CRUD
-    path("dashboard/", dashboard.as_view(), name="dashboard"),
-    path("create-record", create_record.as_view(), name="create-record"),
-    path('update-record/<int:pk>/', update_record.as_view(), name="update-record"),
-    path('record/<int:pk>/', views.view_record.as_view(), name="record"),
-    path('delete-record/<int:pk>', delete_record.as_view(), name="delete-record"),
+    path("dashboard/", Dashboard.as_view(), name="dashboard"),
+    path("create-record", CreateRecord.as_view(), name="create-record"),
+    path('update-record/<int:pk>/', UpdateRecord.as_view(), name="update-record"),
+    path('record/<int:pk>/', views.ViewRecord.as_view(), name="record"),
+    path('delete-record/<int:pk>', DeleteRecord.as_view(), name="delete-record"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
